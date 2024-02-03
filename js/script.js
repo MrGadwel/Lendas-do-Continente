f1aa648ef82a89553b4605a6e20963fb622a168c
MrGadwel <mrgadwel@gmail.com>
2024-02-02 21:16:46 -0300

    Refinamento com cabeçalho, rodape, debug e fanarts

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 var navElement = document.querySelector('.pop-up');
+var footer = document.querySelector('.footer');
+var header = document.querySelector('.header');
 
 
 
@@ -42,39 +44,40 @@ var navElement = document.querySelector('.pop-up');
     <details>
         <summary> <i id="indice"  class="fa-solid fa-book"></i>TODAS AS LENDAS</summary>
             <ul>
-                <li> <i id="indice"  class="fa-solid fa-book"></i><a href="../../../pages/todas-as-lendas/lenda-dentes-e-garras/lenda-dentes-e-garras.html">DENTES E GARRAS</a></li>
-                <li> <i id="indice"  class="fa-solid fa-book"></i><a href="../../../pages/todas-as-lendas/lenda-outras/lenda-outras.html">Outras Lendas</a></li>
+                <li> <i id="indice"  class="fa-solid fa-book"></i><a href="../../../todas-as-lendas/lenda-dentes-e-garras/lenda-dentes-e-garras.html">DENTES E GARRAS</a></li>
+                <li> <i id="indice"  class="fa-solid fa-book"></i><a href="../../../todas-as-lendas/lenda-outras/lenda-outras.html">Outras Lendas</a></li>
             </ul>
     </details>
     <details>
         <summary> <i class="fa-solid fa-shield-halved"></i> ARMAS</summary>
             <ul>
-                <li> <i class="fa-solid fa-eye"></i><a href="../../../pages/armas/espada-claire/espada-claire.html"> Espada</a></li>
+                <li> <i class="fa-solid fa-eye"></i><a href="../../../armas/espada-claire/espada-claire.html"> Espada</a></li>
+                <li> <i class="fa-solid fa-hammer"></i><a href="../../../armas/martelo-de-guerra-anão/martelo-de-guerra-anão.html"> Martelo de Guerra Anão</a></li>
             </ul>
     </details>
     <details>
         <summary> <i class="fa-solid fa-dragon"></i> CRIATURS</summary>
             <ul>
-                <li> <i class="fa-solid fa-eye"></i><a href="../../../pages/criaturas/berserker/berserker.html"> Berserkers</a></li>
+                <li> <i class="fa-solid fa-eye"></i><a href="../../../criaturas/berserker/berserker.html"> Berserkers</a></li>
             </ul>
     </details>
     <details>
         <summary> <i class="fa-regular fa-map"></i> LUGARES</summary>
             <ul>
-                <li> <i class="fa-regular fa-map"></i><a href="../../../pages/lugares/kohan/kohan.html"> Kohan</a></li>
+                <li> <i class="fa-regular fa-map"></i><a href="../../../lugares/kohan/kohan.html"> Kohan</a></li>
             </ul>
     </details>
     <details>
         <summary> <i class="fa-solid fa-square"></i> MATERIAIS</summary>
             <ul>
-                <li> <i class="fa-solid fa-square"></i><a href="../../../pages/materiais/aço negro/aço negro.html"> Aço Negro</a></li>
-                <li> <i class="fa-solid fa-square"></i><a href="../../../pages/materiais/heilig/heilig.html"> Heilig</a></li>
+                <li> <i class="fa-solid fa-square"></i><a href="../../../materiais/aço negro/aço negro.html"> Aço Negro</a></li>
+                <li> <i class="fa-solid fa-square"></i><a href="../../../materiais/heilig/heilig.html"> Heilig</a></li>
             </ul>
     </details>
     <details>
         <summary> <i class="fa-solid fa-newspaper"></i></i> REGISTROS</summary>
             <ul>
-                <li> <i class="fa-solid fa-newspaper"></i><a href="../../../pages/registros/diário de ordock/diário de ordock_01.html"> Diário de Ordock</a></li>
+                <li> <i class="fa-solid fa-newspaper"></i><a href="../../../registros/diário de ordock/diário de ordock_01.html"> Diário de Ordock</a></li>
             </ul>
     </details>
 
@@ -82,6 +85,59 @@ var navElement = document.querySelector('.pop-up');
 
     
     `;
+    var rodape=`
+    
+    <div><H2>&nbsp</H2></div>
+    <table id="tabela_meio"><tr><td>
+
+        <div class="redes_s" id="tabela_meio">
+
+            <a href="https://discord.gg/w3wGyfAT8s" style="background: none;" target="_blank"><img src="../../img/Discord.png" alt="" width="50px"></a>
+            <a href="https://www.instagram.com/lendasdocontinente/" style="background: none;" target="_blank"><img src="../../img/Instagram.png" alt="" width="50px"></a>
+            <a href="https://twitter.com/lendacontinente" style="background: none;" target="_blank"><img src="../../img/X.png" alt="" width="50px"></a>
+
+        </div>
+
+    </td></tr></table>
+
+
+    <div class="foooter">
+    <div class="footer-gad">
+        <H2>LENDAS DO CONTINENTE por Gadwel</H2>
+        <br>
+        <div class="links">
+        
+            <a href="../index.html"><i class='bx bxs-home'></i></a>
+            <a href="https://gadwel.carrd.co/" target="_blank"><i class='bx bx-link-alt'></i></i></a>
+        </div>
+    </div>
+    
+    <div class="dev">
+        <h3>Site desenvolvido por <a class="h-name" href="https://higor-milani.dev/" target="_blank">Higor Milani</a></h3>
+        <div class="dev-img">
+            <a class="h-img"  href="https://higor-milani.dev/" target="_blank"><img src="../../img/higor-milani-dev.svg" alt="Acesse https://higor-milani.dev/"><a>
+            <a class="h-link" href="https://higor-milani.dev/" target="_blank"><h3>Acesse https://higor-milani.dev/</h3></a>
+            
+        </div>
+        <br>
+        <br>
+    </div>
+</div>
+    
+    `;
+    var menutopo=`
+
+    <nav class="nav_cabeçalho">
+            <p style="text-indent:0px;" class="texto_cabeçalho"><b><a href="../../index.html" class="link_menutopo">INÍCIO</a></b></p>
+            <p style="text-indent:0px;" class="texto_cabeçalho"><b><a href="../../todas-as-lendas/lenda-dentes-e-garras/lenda-dentes-e-garras.html" class="link_menutopo">DENTES E GARRAS</a></b></p>
+            <p style="text-indent:0px;" class="texto_cabeçalho"><b><a href="../../fanarts/fanarts.html" class="link_menutopo">FANARTS</a></b></p>
+            <p style="text-indent:0px;" class="texto_cabeçalho"><b><a href="../../_debug/_debug.html" class="link_menutopo">?</a></b></p>
+    </nav>
+    
+    
+    `
+
+
 
     ////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -89,6 +145,8 @@ var navElement = document.querySelector('.pop-up');
 
     // Atualização do conteúdo do <nav>
     navElement.innerHTML = novoConteudo;
+    footer.innerHTML = rodape;
+    header.innerHTML = menutopo;
 
 
 
